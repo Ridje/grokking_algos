4b8e6076c47f265471ccb1611bcf5a6bbfcc5bc3
i.kazantsevi <i.kazantsev@andersenlab.com>
2024-05-27 00:50:50 +0400

    q_sort_random_pivot

--- src/main/kotlin/QSort.kt
+++ src/main/kotlin/QSort.kt
@@ -1,11 +1,13 @@
+import kotlin.random.Random
+
 class QSort {
     fun qSort(array: IntArray): IntArray {
         if (array.size < 2) {
             return array
         }
-        val pivot = array[0]
-        val less = array.drop(1).filter { it <= pivot }.toIntArray()
-        val greater = array.drop(1).filter { it > pivot }.toIntArray()
+        val pivot = array[Random.nextInt(0, array.size)]
+        val less = array.filter { it < pivot }.toIntArray()
+        val greater = array.filter { it > pivot }.toIntArray()
         return qSort(less) + pivot + qSort(greater)
     }
 }
